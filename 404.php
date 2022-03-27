ff6df86662beca3196264d9fcb932c3bc916cb92
Artur <art.pipchenko@gmail.com>
2022-03-27 13:36:15 +0300

    added styling and content for 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,11 @@
-<?php ?>
-<div class="404">
-  sorry, nothing found
+<?php get_header(); ?>
+<div class="error404">
+  <?php echo zhm_get_title('404'); ?>
+  <h3 class="bold primary-font text-primary mt-5 mb-3">
+    Сторінка, яку Ви шукаєте, не знайдена або більше не існує. 
+  </h3>
+  <div class="text-primary secondary-fotn pt-3 error404-description">
+    Будь ласка, поверніться на головну або скористуйтеся формою пошуку для уточнення Вашого запиту
+  </div>
 </div>
-<?php ?>
+<?php get_footer(); ?>
