49128ceceadd24a3963e1357b044e67f6cd18eb6
Artur <art.pipchenko@gmail.com>
2022-04-09 15:55:20 +0300

    - search schema.org - metaboxes for zhm_doc - template for docs search

---
+++ page-templates/page-files.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Template Name: Сторінка файлів
+*/
+?>
+
+<?php get_header(); ?>
+  <?php get_template_part('breadcrumbs'); ?>
+  <div class="row mb-3">
+    <div class="col-md-9">
+      <?php echo zhm_get_title('Новини'); ?>
+      <?php
+        $current_page = get_query_var('paged');
+
+        $news_query = new WP_Query(array(
+          'post_type' => 'zhm_doc',
+          'posts_per_page' => 5,
+          'paged' => $current_page
+        ));
+
+        if ($news_query->have_posts()) {
+          while ($news_query->have_posts()) {
+            $post = get_post($news_query->the_post());
+
+            get_template_part( 'post', 'preview', array('post' => $post) );
+          } 
+      ?>
+      <div class="news-pagination bold primary-font text-primary">
+        <?php
+          echo paginate_links(array(
+            'total' => $news_query->max_num_pages,
+            'prev_text' => 'Попередні',
+            'next_text' => 'Наступні'
+          ));
+        ?>
+      </div>
+      <?php
+        } else {
+      ?>
+        <h3 class="primary-font text-primary mt-3">
+          Новин поки що немає. Будь ласка, зайдіть пізніше.
+        </h3>
+      <?php
+        }
+      ?>
+    </div>
+    <?php get_template_part('side', 'menu'); ?>
+  </div>
+<?php get_footer(); ?>
